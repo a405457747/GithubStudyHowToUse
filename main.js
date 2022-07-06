b1f5b4c4c2b1073e222512d4981057d1d6117898
Sky.Allen <894982165@qq.com>
2022-07-06 15:35:18 +0800

    Squashed commit of the following:
    commit 2cf3d18e7db7d20f8b3d9c1db95a185c6464de30
    Author: Sky.Allen <894982165@qq.com>
    Date:   Wed Jul 6 15:32:02 2022 +0800
    
        1
    
    commit 633d554f88327deae433590795d3e3a81b504627
    Author: Sky.Allen <894982165@qq.com>
    Date:   Wed Jul 6 15:31:36 2022 +0800
    
        1

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
+let k33=addx(33,22);
 
 let k =addx(3,5);
 
 function cheng(x,y){
-    return x*y+2+x*y;
+    return x*y+2+x*y+3334;
 }
-let j =cheng(3,5);
 // helloworld
 // hellosiki
 let k2 = cheng(22,33);
+//wonima
